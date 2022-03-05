22e01ee2461fd888a96128ececcf08ee8f8451e6
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-03-05 13:21:42 +0800

    ci: download artifact for code browser according to specify scenario

--- .github/workflows/CI.sh
+++ .github/workflows/CI.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+PROJECT_FOLDER="foo"
+BROWSER_FOLDER="browser"
+NETRC_FILE=".netrc"
+ARTIFACT_FILE="foo_artifact"
+ARTIFACT_URL="https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=1"
+SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.2/shfmt_v3.4.2_linux_amd64"
+SHFMT_ORIGINAL="shfmt_v3.4.2_linux_amd64"
+WOBOQ_URL="https://github.com/KDAB/codebrowser.git"
+WOBOQ_COMMIT="73fce32fc696b3f6eb2a678397328d9ce1ad4cf6"
+
 shCommand()
 {
     echo
@@ -10,23 +20,71 @@ shCommand()
 
 printAbort()
 {
-    echo "Shell script build.sh: $*"
+    echo "Shell script CI.sh: $*"
     exit 1
+}
+
+installDependencies()
+{
+    shCommand "sudo apt-get install -y llvm-10 clang-10 libclang-10-dev \
+clang-format-10 clang-tidy-10 cmake python3 pylint black shellcheck valgrind"
+    shCommand "curl -O ${SHFMT_URL} && sudo mv ${SHFMT_ORIGINAL} shfmt \
+&& sudo mv shfmt /usr/local/bin/ && sudo chmod +x /usr/local/bin/shfmt"
+    shCommand "git clone ${WOBOQ_URL} && cd ./codebrowser && git reset --hard ${WOBOQ_COMMIT} \
+&& cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-10 && make -j4 \
+&& sudo make install && cd .. && rm -rf ./codebrowser"
+}
+
+downloadArtifact()
+{
+    if [ ! -f ~/"${NETRC_FILE}" ]; then
+        printAbort "There is no ${NETRC_FILE} file in ~/ folder."
+    fi
+    shCommand "git remote -v update"
+    localCommitId=$(git rev-parse @)
+    remoteCommitId=$(git rev-parse @\{u\})
+    if [ "${localCommitId}" != "${remoteCommitId}" ]; then
+        shCommand "git pull --rebase"
+    else
+        printAbort "No change in ${PROJECT_FOLDER} project."
+    fi
+
+    files=$(find ~/"${BROWSER_FOLDER}" ./* 2>/dev/null | sed 1d | wc -l)
+    if [ "${files}" != "0" ]; then
+        shCommand "rm -rf ~/${BROWSER_FOLDER}/*"
+    fi
+    actionUrl=$(curl -s "${ARTIFACT_URL}" \
+        | jq '[.artifacts[] | {name : .name, archive_download_url : .archive_download_url}]' \
+        | jq -r '.[] | select (.name == "'"${ARTIFACT_URL}"'") | .archive_download_url' | head -n1)
+    localUrl=$(curl -netrc -silent "${actionUrl}" | grep -oP 'location: \K.*' | tr -d '\r')
+    shCommand "curl -L -X GET \"${localUrl}\" --output ~/${BROWSER_FOLDER}/${ARTIFACT_FILE}.zip"
+    if
+        zip -T ~/"${BROWSER_FOLDER}"/"${ARTIFACT_FILE}".zip | grep 'zip error' >/dev/null 2>&1
+    then
+        printAbort "The zip file ${ARTIFACT_FILE}.zip in ~/${BROWSER_FOLDER} folder is corrupted."
+    else
+        htmlFolder="${PROJECT_FOLDER}_html"
+        shCommand shCommand "unzip ~/${BROWSER_FOLDER}/${ARTIFACT_FILE}.zip -d ~/${BROWSER_FOLDER}"
+        shCommand "tar -jxvf ~/${BROWSER_FOLDER}/${htmlFolder}_*.tar.bz2 -C ~/${BROWSER_FOLDER} \
+>/dev/null"
+        shCommand "rm -rf ~/${BROWSER_FOLDER}/*.zip ~/${BROWSER_FOLDER}/*.tar.bz2"
+    fi
 }
 
 main()
 {
     cd "$(dirname "$0")" || exit 1
+    localEnv=$(git rev-parse --show-toplevel)
+    cd "${localEnv}" || exit 1
+    ciEnv="/home/runner/work/${PROJECT_FOLDER}/${PROJECT_FOLDER}"
 
-    shCommand "sudo apt-get install -y llvm-10 clang-10 libclang-10-dev \
-clang-format-10 clang-tidy-10 cmake python3 pylint black shellcheck valgrind"
-    shCommand "wget https://github.com/mvdan/sh/releases/download/v3.4.2/shfmt_v3.4.2_linux_amd64 \
-&& sudo mv shfmt_v3.4.2_linux_amd64 shfmt && sudo mv shfmt /usr/local/bin/ \
-&& sudo chmod +x /usr/local/bin/shfmt"
-    shCommand "git clone https://github.com/KDAB/codebrowser.git && cd ./codebrowser \
-&& git reset --hard 73fce32fc696b3f6eb2a678397328d9ce1ad4cf6 \
-&& cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-10 && make -j4 \
-&& sudo make install && cd .."
+    if [ "${localEnv}" = "${ciEnv}" ]; then
+        echo "$(date "+%b %d %T") INSTALL DEPENDENCIES"
+        installDependencies
+    elif [ -d ~/"${BROWSER_FOLDER}" ]; then
+        echo "$(date "+%b %d %T") DOWNLOAD ARTIFACT"
+        downloadArtifact
+    fi
 }
 
 main "$@"
