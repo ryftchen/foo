7df23ee835a1c298d249dc8d5920e9aebfbbd04c
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-03-05 19:26:03 +0800

    fix: unable to get the expected commit id from remote

--- .github/workflows/CI.sh
+++ .github/workflows/CI.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-GITHUB_FOLDER="/home/runner/work"
+GITHUB_PATH="/home/runner/work"
+PROJECT_URL="https://github.com/ryftchen/foo.git"
 PROJECT_FOLDER="foo"
 BROWSER_FOLDER="browser"
 NETRC_FILE=".netrc"
-ARTIFACT_FILE="foo_artifact"
 ARTIFACT_URL="https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=1"
+ARTIFACT_FILE="foo_artifact"
 SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.2/shfmt_v3.4.2_linux_amd64"
-SHFMT_ORIGINAL="shfmt_v3.4.2_linux_amd64"
+SHFMT_FILE="shfmt_v3.4.2_linux_amd64"
 WOBOQ_URL="https://github.com/KDAB/codebrowser.git"
 WOBOQ_COMMIT="73fce32fc696b3f6eb2a678397328d9ce1ad4cf6"
 
@@ -29,7 +30,7 @@ installDependencies()
 {
     shCommand "sudo apt-get install -y llvm-10 clang-10 libclang-10-dev \
 clang-format-10 clang-tidy-10 cmake python3 pylint black shellcheck valgrind"
-    shCommand "curl -O ${SHFMT_URL} && sudo mv ${SHFMT_ORIGINAL} shfmt \
+    shCommand "curl -O ${SHFMT_URL} && sudo mv ${SHFMT_FILE} shfmt \
 && sudo mv shfmt /usr/local/bin/ && sudo chmod +x /usr/local/bin/shfmt"
     shCommand "git clone ${WOBOQ_URL} && cd ./codebrowser && git reset --hard ${WOBOQ_COMMIT} \
 && cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-10 && make -j4 \
@@ -41,9 +42,8 @@ downloadArtifact()
     if [ ! -f ~/"${NETRC_FILE}" ]; then
         printAbort "There is no ${NETRC_FILE} file in ~/ folder."
     fi
-    shCommand "git remote -v update"
-    localCommitId=$(git rev-parse @)
-    remoteCommitId=$(git rev-parse @\{u\})
+    localCommitId=$(git rev-parse HEAD)
+    remoteCommitId=$(git ls-remote "${PROJECT_URL}" refs/heads/master | cut -f 1)
     htmlFolder="${PROJECT_FOLDER}_html"
     if [ "${localCommitId}" != "${remoteCommitId}" ]; then
         shCommand "git pull origin master"
@@ -78,7 +78,7 @@ main()
     cd "$(dirname "$0")" || exit 1
     localEnv=$(git rev-parse --show-toplevel)
     cd "${localEnv}" || exit 1
-    ciEnv="${GITHUB_FOLDER}/${PROJECT_FOLDER}/${PROJECT_FOLDER}"
+    ciEnv="${GITHUB_PATH}/${PROJECT_FOLDER}/${PROJECT_FOLDER}"
 
     if [ "${localEnv}" = "${ciEnv}" ]; then
         echo "$(date "+%b %d %T") INSTALL DEPENDENCIES"
