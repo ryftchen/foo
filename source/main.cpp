8c974e2609ec20c6a1a4301a0e79700672ac8982
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-03-05 19:53:11 +0800

    refactor: use __attribute__ to switch path

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,8 @@
 #include "../include/main.hpp"
-#include <iostream>
 #include "../include/command.hpp"
-
-using namespace std;
 
 int main(int argc, char *argv[])
 {
-    switchToProjectPath();
-
     const std::shared_ptr<Command> cmd = std::make_shared<Command>();
     if (cmd->parseArgv(argc - 1, argv + 1))
     {
