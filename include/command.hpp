dfaf427f031b312ccfa00ce37129544b1a3c7e92
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-03-07 22:42:18 +0800

    refactor: use the mutable keyword and the volatile keyword

--- include/command.hpp
+++ include/command.hpp
@@ -70,8 +70,19 @@ public:
     void doTask();
 
 private:
-    std::mutex commandMutex;
-    typedef void (Command::*TaskFunctor)();
+#pragma pack(8)
+    struct TaskPlan
+    {
+        std::bitset<OptimumBit::optimumButtom> optimumBit;
+        std::bitset<IntegralBit::integralButtom> integralBit;
+        std::bitset<SortBit::sortButtom> sortBit;
+        volatile bool taskDone = false;
+
+        TaskPlan() = default;
+    } run;
+#pragma pack()
+    mutable std::mutex commandMutex;
+    typedef void (Command::*TaskFunctor)() const;
     const TaskFunctor taskFunctor[TaskBit::taskButtom] = {
         &Command::runOptimum,
         &Command::runIntegral,
@@ -80,33 +91,22 @@ private:
         {"o_fib", "o_gra", "o_ann", "o_par", "o_gen"},
         {"i_tra", "i_sim", "i_rom", "i_gau", "i_mon"},
         {"s_bub", "s_sec", "s_ins", "s_she", "s_mer", "s_qui", "s_hea", "s_cou", "s_buc", "s_rad"}};
-    // #pragma pack(8)
-    struct TaskPlan
-    {
-        std::bitset<OptimumBit::optimumButtom> optimumBit;
-        std::bitset<IntegralBit::integralButtom> integralBit;
-        std::bitset<SortBit::sortButtom> sortBit;
-        bool taskDone = false;
-
-        TaskPlan() = default;
-    } run;
-    // #pragma pack()
     void setBitFromTaskPlan(char *const argv[], const std::bitset<TaskBit::taskButtom> &taskBit);
-    void runOptimum();
+    void runOptimum() const;
     void getOptimumResult(
         const Expression &express,
         const double leftEndpoint,
         const double rightEndpoint,
         const double epsilon) const;
     void setOptimumBit(char *const argv[]);
-    void runIntegral();
+    void runIntegral() const;
     void getIntegralResult(
         const Expression &express,
         const double lowerLimit,
         const double upperLimit,
         const double epsilon) const;
     void setIntegralBit(char *const argv[]);
-    void runSort();
+    void runSort() const;
     void getSortResult(const std::shared_ptr<Sort<int>> sort) const;
     void setSortBit(char *const argv[]);
     friend void printFile(const char *const pathname, const bool reverse, const uint32_t maxLine);
