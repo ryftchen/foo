89716a8debe4a586fe2a8bfdc6e529d1db43a286
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-03-18 23:54:04 +0800

    refactor: separate download artifact function from ci.sh, use requests & json module instead of curl & jq

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-PROJECT_URL="https://github.com/ryftchen/foo.git"
-PROJECT_FOLDER="foo"
-BROWSER_FOLDER="browser"
-NETRC_FILE=".netrc"
-ARTIFACT_URL="https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=1"
-ARTIFACT_FILE="foo_artifact"
 SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.2/shfmt_v3.4.2_linux_amd64"
-WOBOQ_URL="https://github.com/KDAB/codebrowser.git"
+WOBOQ_GIT="https://github.com/KDAB/codebrowser.git"
 WOBOQ_COMMIT="73fce32fc696b3f6eb2a678397328d9ce1ad4cf6"
 
 shCommand()
 {
     echo
-    echo "$(date "+%b %d %T") $*" BEGIN
+    echo "$(date "+%b %d %T") $* BEGIN"
     sh -c "$@"
-    echo "$(date "+%b %d %T") $*" END
+    echo "$(date "+%b %d %T") $* END"
 }
 
 printAbort()
@@ -26,77 +20,33 @@ printAbort()
 
 installDependencies()
 {
-    echo
-    echo "$(date "+%b %d %T") TRIGGER INSTALL DEPENDENCIES"
+    cd "$(dirname "$0")" || exit 1
+    localDir=$(git rev-parse --show-toplevel)
+    cd "${localDir}" || exit 1
 
     shCommand "sudo apt-get install -y llvm-10 clang-10 libclang-10-dev \
 clang-format-10 clang-tidy-10 cmake python3 pylint black shellcheck global valgrind"
     shCommand "sudo curl -L ${SHFMT_URL} >/usr/local/bin/shfmt \
 && sudo chmod +x /usr/local/bin/shfmt"
-    shCommand "git clone ${WOBOQ_URL} && cd ./codebrowser && git reset --hard ${WOBOQ_COMMIT} \
+    shCommand "git clone ${WOBOQ_GIT} -b master \
+&& cd ./codebrowser && git reset --hard ${WOBOQ_COMMIT} \
 && cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-10 && make -j4 \
 && sudo make install && cd .. && rm -rf ./codebrowser"
 }
 
-downloadArtifact()
-{
-    echo
-    echo "$(date "+%b %d %T") TRIGGER DOWNLOAD ARTIFACT"
-
-    if [ ! -d ~/"${BROWSER_FOLDER}" ]; then
-        printAbort "Please create ${BROWSER_FOLDER} folder in ~/ folder."
-    fi
-    if [ ! -f ~/"${NETRC_FILE}" ]; then
-        printAbort "There is no ${NETRC_FILE} file in ~/ folder."
-    fi
-    localCommitId=$(git rev-parse HEAD)
-    remoteCommitId=$(git ls-remote "${PROJECT_URL}" refs/heads/master | cut -f 1)
-    htmlFolder="${PROJECT_FOLDER}_html"
-    if [ -n "${remoteCommitId}" ]; then
-        if [ "${localCommitId}" != "${remoteCommitId}" ]; then
-            shCommand "git pull origin master"
-        elif [ -d ~/"${BROWSER_FOLDER}"/"${htmlFolder}" ]; then
-            printAbort "No change in ${PROJECT_FOLDER} project."
-        fi
-    else
-        printAbort "Failed to get the latest commit id."
-    fi
-
-    actionUrl=$(curl -s "${ARTIFACT_URL}" \
-        | jq '[.artifacts[] | {name : .name, archive_download_url : .archive_download_url}]' \
-        | jq -r '.[] | select (.name == "'"${ARTIFACT_FILE}"'") | .archive_download_url' | head -n1)
-    localUrl=$(curl -netrc -silent "${actionUrl}" | grep -oP 'location: \K.*' | tr -d '\r')
-    shCommand "curl -L -X GET \"${localUrl}\" --output ~/${BROWSER_FOLDER}/${ARTIFACT_FILE}.zip"
-    if
-        zip -T ~/"${BROWSER_FOLDER}"/"${ARTIFACT_FILE}".zip | grep 'zip error' >/dev/null 2>&1
-    then
-        shCommand "rm -rf ~/${BROWSER_FOLDER}/${ARTIFACT_FILE}.zip"
-        shCommand "git reset --hard ${localCommitId}"
-        printAbort "The zip file ${ARTIFACT_FILE}.zip in ~/${BROWSER_FOLDER} folder is corrupted."
-    else
-        shCommand "rm -rf ~/${BROWSER_FOLDER}/${htmlFolder}"
-        shCommand "unzip ~/${BROWSER_FOLDER}/${ARTIFACT_FILE}.zip -d ~/${BROWSER_FOLDER}"
-        shCommand "tar -jxvf ~/${BROWSER_FOLDER}/${htmlFolder}_*.tar.bz2 -C ~/${BROWSER_FOLDER} \
->/dev/null"
-        shCommand "rm -rf ~/${BROWSER_FOLDER}/*.zip ~/${BROWSER_FOLDER}/*.tar.bz2"
-    fi
-}
-
 main()
 {
-    cd "$(dirname "$0")" || exit 1
-    localDir=$(git rev-parse --show-toplevel)
-    cd "${localDir}" || exit 1
-
+    echo
+    echo "$(date "+%b %d %T") ----- INSTALL DEPENDENCIES BEGIN -----"
     if [ -n "${FOO_ENV:?}" ]; then
         if [ "${FOO_ENV}" = "GITHUB_ACTION" ]; then
             installDependencies
-        elif [ "${FOO_ENV}" = "CODE_BROWSER" ]; then
-            downloadArtifact
         fi
     else
         printAbort "Please set environment variable FOO_ENV."
     fi
+    echo
+    echo "$(date "+%b %d %T") -----  INSTALL DEPENDENCIES END  -----"
 }
 
 main "$@"
---
+++ .github/workflows/download.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import subprocess
+import sys
+from datetime import datetime
+import requests
+
+PROJECT_GIT = "https://github.com/ryftchen/foo.git"
+PROJECT_FOLDER = "foo"
+BROWSER_FOLDER = "browser"
+ARTIFACT_URL = "https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=1"
+ARTIFACT_FILE = "foo_artifact"
+HOME = os.path.expanduser("~")
+
+
+def executeCommand(command, output=True):
+    if output:
+        print(
+            "\r\n{} {} BEGIN".format(datetime.strftime(datetime.now(), "%b %d %H:%M:%S"), command)
+        )
+    try:
+        cmd = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8"
+        )
+        cmd.wait()
+    except RuntimeError():
+        printAbort(f"Failed to execute command: \"{command}\".")
+    if output:
+        out, err = cmd.communicate()
+        if len(out) != 0:
+            print(out)
+        elif len(err) != 0:
+            print(err)
+        print("{} {} END".format(datetime.strftime(datetime.now(), "%b %d %H:%M:%S"), command))
+    return cmd
+
+
+def printAbort(message):
+    print(f"Python script download.py: {message}")
+    sys.exit(-1)
+
+
+def downloadArtifact():
+    os.chdir(os.path.split(os.path.realpath(__file__))[0])
+    localDir = (
+        executeCommand("git rev-parse --show-toplevel", output=False).stdout.read().splitlines()[0]
+    )
+    os.chdir(localDir)
+
+    if not os.path.exists(f"{HOME}/{BROWSER_FOLDER}"):
+        printAbort(f"Please create {BROWSER_FOLDER} in ~/ folder.")
+    localCommitId = executeCommand("git rev-parse HEAD", output=False).stdout.read().splitlines()[0]
+    remoteCommitId = (
+        executeCommand(f"git ls-remote {PROJECT_GIT} refs/heads/master | cut -f 1", output=False)
+        .stdout.read()
+        .splitlines()[0]
+    )
+    htmlFolder = f"{PROJECT_FOLDER}_html"
+    if len(remoteCommitId) != 0:
+        if localCommitId != remoteCommitId:
+            executeCommand("git pull origin master")
+        elif os.path.exists(f"{HOME}/{BROWSER_FOLDER}/{htmlFolder}"):
+            printAbort(f"No change in {PROJECT_FOLDER} project.")
+    else:
+        printAbort("Failed to get the latest commit id.")
+
+    try:
+        response = requests.get(ARTIFACT_URL)
+        response.raise_for_status()
+
+        jsonInfo = json.loads(response.text)
+        if jsonInfo["total_count"] != 0:
+            if jsonInfo["artifacts"][0]["name"] == ARTIFACT_FILE:
+                actionUrl = jsonInfo["artifacts"][0]["archive_download_url"]
+
+        response = requests.get(actionUrl, allow_redirects=False)
+        response.raise_for_status()
+        locationUrl = response.headers["location"]
+
+        response = requests.get(locationUrl)
+        response.raise_for_status()
+        with open(f"{HOME}/{BROWSER_FOLDER}/{ARTIFACT_FILE}.zip", "wb") as outputFile:
+            outputFile.write(response.content)
+    except requests.exceptions.HTTPError as error:
+        printAbort(error)
+
+    zipCheck = executeCommand(
+        f"zip -T ~/{BROWSER_FOLDER}/{ARTIFACT_FILE}.zip", output=False
+    ).stdout.read()
+    if zipCheck.find("zip error") == -1:
+        executeCommand(f"rm -rf ~/{BROWSER_FOLDER}/{htmlFolder}")
+        executeCommand(f"unzip ~/{BROWSER_FOLDER}/{ARTIFACT_FILE}.zip -d ~/{BROWSER_FOLDER}")
+        executeCommand(
+            f"tar -jxvf ~/{BROWSER_FOLDER}/{htmlFolder}_*.tar.bz2 -C ~/{BROWSER_FOLDER} >/dev/null"
+        )
+        executeCommand(f"rm -rf ~/{BROWSER_FOLDER}/*.zip ~/{BROWSER_FOLDER}/*.tar.bz2")
+    else:
+        executeCommand(f"rm -rf ~/{BROWSER_FOLDER}/{ARTIFACT_FILE}.zip")
+        executeCommand(f"git reset --hard {localCommitId}")
+        printAbort(f"The zip file {ARTIFACT_FILE}.zip in ~/{BROWSER_FOLDER} folder is corrupted.")
+
+
+if __name__ == "__main__":
+    print(
+        "\r\n{} ----- DOWNLOAD ARTIFACT BEGIN -----".format(
+            datetime.strftime(datetime.now(), "%b %d %H:%M:%S")
+        )
+    )
+    if os.getenv("FOO_ENV") == "CODE_BROWSER":
+        downloadArtifact()
+    else:
+        printAbort("Please set environment variable FOO_ENV.")
+    print(
+        "\r\n{} -----  DOWNLOAD ARTIFACT END  -----".format(
+            datetime.strftime(datetime.now(), "%b %d %H:%M:%S")
+        )
+    )
