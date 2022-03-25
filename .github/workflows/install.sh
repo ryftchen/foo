1fa0c1b2b93e24f967be04ff6fc90850a9f40fff
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-03-26 00:06:29 +0800

    style: improve readability

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-STATUS_SPLIT_LINE="-----"
-STATUS_INSTALL_DEPENDENCIES="INSTALL DEPENDENCIES"
 SHFMT_URL="https://github.com/mvdan/sh/releases/download/v3.4.2/shfmt_v3.4.2_linux_amd64"
+SHFMT_PATH="/usr/local/bin"
 WOBOQ_GIT="https://github.com/KDAB/codebrowser.git"
 WOBOQ_COMMIT="73fce32fc696b3f6eb2a678397328d9ce1ad4cf6"
+WOBOQ_FOLDER="codebrowser"
 
 bashCommand()
 {
@@ -28,19 +28,19 @@ installDependencies()
 
     bashCommand "sudo apt-get install -y llvm-11 clang-11 libclang-11-dev \
 clang-format-11 clang-tidy-11 cmake python3 pylint black shellcheck valgrind global sysvbanner"
-    bashCommand "sudo curl -L ${SHFMT_URL} >/usr/local/bin/shfmt \
-&& sudo chmod +x /usr/local/bin/shfmt"
+    bashCommand "sudo curl -L ${SHFMT_URL} >${SHFMT_PATH}/shfmt \
+&& sudo chmod +x ${SHFMT_PATH}/shfmt"
     bashCommand "git clone ${WOBOQ_GIT} -b master \
-&& cd ./codebrowser && git reset --hard ${WOBOQ_COMMIT} \
-&& cmake . -DCMAKE_CXX_COMPILER=clang++-11 -DCMAKE_BUILD_TYPE=Release && make -j4 \
-&& sudo make install && cd .. && rm -rf ./codebrowser"
+&& git -C ./${WOBOQ_FOLDER} reset --hard ${WOBOQ_COMMIT} \
+&& cmake -S ./${WOBOQ_FOLDER} -B ./${WOBOQ_FOLDER} \
+-DCMAKE_CXX_COMPILER=clang++-11 -DCMAKE_BUILD_TYPE=Release && make -C ./${WOBOQ_FOLDER} -j \
+&& sudo make -C ./${WOBOQ_FOLDER} install && rm -rf ./${WOBOQ_FOLDER}"
 }
 
 main()
 {
     echo
-    echo "$(date "+%b %d %T") \
-${STATUS_SPLIT_LINE} ${STATUS_INSTALL_DEPENDENCIES} BEGIN ${STATUS_SPLIT_LINE}"
+    echo "$(date "+%b %d %T") ---------- INSTALL DEPENDENCIES BEGIN"
 
     if [ -n "${FOO_ENV:?}" ]; then
         if [ "${FOO_ENV}" = "GITHUB_ACTION" ]; then
@@ -51,8 +51,7 @@ ${STATUS_SPLIT_LINE} ${STATUS_INSTALL_DEPENDENCIES} BEGIN ${STATUS_SPLIT_LINE}"
     fi
 
     echo
-    echo "$(date "+%b %d %T") \
-${STATUS_SPLIT_LINE}  ${STATUS_INSTALL_DEPENDENCIES} END  ${STATUS_SPLIT_LINE}"
+    echo "$(date "+%b %d %T") ---------- INSTALL DEPENDENCIES END "
 }
 
 main "$@"
