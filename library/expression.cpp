d1692c5d56c0878d2446aefa3375353042f80abe
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-04-17 12:55:54 +0800

    style: expand macro

--- library/expression.cpp
+++ library/expression.cpp
@@ -2,24 +2,24 @@
 
 double Function1::operator()(const double x) const
 {
-    return EXPRESSION_FUN_1;
+    return ((x * std::sin(x)) / (1.0 + std::cos(x) * std::cos(x)));
 }
 
 double Function2::operator()(const double x) const
 {
-    return EXPRESSION_FUN_2;
+    return (x + 10.0 * std::sin(5.0 * x) + 7.0 * std::cos(4.0 * x));
 }
 
 #ifdef EXPRESSION_NO_MAXIMUM
 // f(X)=1+1/4000*Σ(1→n)[(Xi)^2]-Π(1→n)[cos(Xi/(i)^(1/2))],x∈[-600,600],f(min)=0
 double Griewank::operator()(const double x) const
 {
-    return EXPRESSION_GRI * rate;
+    return (1.0 + 1.0 / 4000.0 * x * x - std::cos(x)) * rate;
 }
 
 // f(x)=An+Σ(1→n)[(Xi)^2-Acos(2π*Xi)],A=10,x∈[-5.12,5.12],f(min)=0
 double Rastrigin::operator()(const double x) const
 {
-    return EXPRESSION_RAS * rate;
+    return (x * x - 10.0 * std::cos(2.0 * M_PI * x) + 10.0) * rate;
 }
 #endif
