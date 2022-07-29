011e348b4041353474603832c6f99c240bd4edcf
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-07-29 16:28:14 +0800

    test: add common.py

---
+++ script/common.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+import curses
+import fcntl
+import signal
+import sys
+import subprocess
+import time
+
+BAR_SAVE_CURSOR = "\033[s"
+BAR_RESTORE_CURSOR = "\033[u"
+BAR_MOVE_UP_CURSOR = "\033[1A"
+BAR_FORE_COLOR = "\033[30m"
+BAR_BACK_COLOR = "\033[42m"
+BAR_FORE_COLOR_DEFAULT = "\033[39m"
+BAR_BACK_COLOR_DEFAULT = "\033[49m"
+BAR_CURRENT_LINES = 0
+BAR_PLACEHOLDER_LENGTH = 20
+BAR_SET_TRAP = False
+BAR_SIGNAL_DEFAULT = None
+STDOUT_DEFAULT = sys.stdout
+
+
+class Log:
+    def __init__(self, filename, stream=sys.stdout):
+        self.terminal = stream
+        self.log = open(filename, "w")
+        fcntl.flock(self.log, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+    def write(self, message):
+        self.terminal.write(message)
+        try:
+            self.log.write(message)
+        except IOError:
+            fcntl.flock(self.log, fcntl.LOCK_UN)
+            self.log.close()
+            sys.stdout = STDOUT_DEFAULT
+            print(f"Python script common.py: Failed to write log file.")
+            sys.exit(-1)
+
+    def flush(self):
+        pass
+
+    def uninit(self):
+        fcntl.flock(self.log, fcntl.LOCK_UN)
+        self.log.close()
+        sys.stdout = STDOUT_DEFAULT
+
+
+def executeCommand(command, output=True):
+    try:
+        cmd = subprocess.Popen(
+            command,
+            shell=True,
+            executable="/bin/bash",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf-8",
+        )
+        cmd.wait()
+    except RuntimeError():
+        print(f"Python script common.py: Failed to execute command: \"{command}\".")
+        sys.exit(-1)
+    if output:
+        out, err = cmd.communicate()
+        if len(out) != 0:
+            print(out)
+        if len(err) != 0:
+            print(err)
+    return cmd
+
+
+def setupProgressBar():
+    global BAR_CURRENT_LINES
+    curses.setupterm()
+
+    trapDueToInterrupt()
+
+    BAR_CURRENT_LINES = tputLines()
+    lines = BAR_CURRENT_LINES - 1
+    printProgress("\n")
+
+    printProgress(BAR_SAVE_CURSOR)
+    printProgress(f"\033[0;{str(lines)}r")
+
+    printProgress(BAR_RESTORE_CURSOR)
+    printProgress(BAR_MOVE_UP_CURSOR)
+    drawProgressBar(0)
+
+
+def drawProgressBar(percentage):
+    lines = tputLines()
+    if lines != BAR_CURRENT_LINES:
+        setupProgressBar()
+
+    printProgress(BAR_SAVE_CURSOR)
+    printProgress(f"\033[{str(lines)};0f")
+
+    tput()
+    printBar(percentage)
+    printProgress(BAR_RESTORE_CURSOR)
+    time.sleep(0.05)
+
+
+def destroyProgressBar():
+    lines = tputLines()
+    printProgress(BAR_SAVE_CURSOR)
+    printProgress(f"\033[0;{str(lines)}r")
+
+    printProgress(BAR_RESTORE_CURSOR)
+    printProgress(BAR_MOVE_UP_CURSOR)
+
+    clearProgressBar()
+    printProgress("\n\n")
+    if BAR_SET_TRAP:
+        signal.signal(signal.SIGINT, BAR_SIGNAL_DEFAULT)
+
+
+def clearProgressBar():
+    lines = tputLines()
+    printProgress(BAR_SAVE_CURSOR)
+    printProgress(f"\033[{str(lines)};0f")
+
+    tput()
+    printProgress(BAR_RESTORE_CURSOR)
+
+
+def trapDueToInterrupt():
+    global BAR_SET_TRAP
+    global BAR_SIGNAL_DEFAULT
+    BAR_SET_TRAP = True
+    BAR_SIGNAL_DEFAULT = signal.getsignal(signal.SIGINT)
+    signal.signal(signal.SIGINT, clearDueToInterrupt)
+
+
+def clearDueToInterrupt(sign, frame):
+    destroyProgressBar()
+    raise KeyboardInterrupt
+
+
+def printBar(percentage):
+    cols = tputCols()
+    barSize = cols - BAR_PLACEHOLDER_LENGTH
+    color = f"{BAR_FORE_COLOR}{BAR_BACK_COLOR}"
+    defaultColor = f"{BAR_FORE_COLOR_DEFAULT}{BAR_BACK_COLOR_DEFAULT}"
+
+    completeSize = int((barSize * percentage) / 100)
+    remainderSize = barSize - completeSize
+    progressBar = f"[{color}{'#' * int(completeSize)}{defaultColor}{'.' * int(remainderSize)}]"
+    printProgress(f" Progress {percentage:>3}% {progressBar}\r")
+
+
+def printProgress(text):
+    print(text, end="")
+
+
+def tputLines():
+    cmd = executeCommand("tput lines", output=False)
+    out = cmd.stdout.read()
+    return int(out)
+
+
+def tputCols():
+    cmd = executeCommand("tput cols", output=False)
+    out = cmd.stdout.read()
+    return int(out)
+
+
+def tput():
+    print(curses.tparm(curses.tigetstr("el")).decode(), end="")
--- script/test.py
+++ script/test.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
-import curses
-import fcntl
 import os
 import re
-import signal
-import subprocess
 import sys
-import time
 from datetime import datetime
+import common
 
 RUN_CMD = "foo"
 RUN_DIR = "./build/"
@@ -53,164 +49,6 @@ ALIGN_CMD = 10
 
 STDOUT_DEFAULT = sys.stdout
 STDOUT_LOG = sys.stdout
-BAR_SAVE_CURSOR = "\033[s"
-BAR_RESTORE_CURSOR = "\033[u"
-BAR_MOVE_UP_CURSOR = "\033[1A"
-BAR_FORE_COLOR = "\033[30m"
-BAR_BACK_COLOR = "\033[42m"
-BAR_FORE_COLOR_DEFAULT = "\033[39m"
-BAR_BACK_COLOR_DEFAULT = "\033[49m"
-BAR_CURRENT_LINES = 0
-BAR_PLACEHOLDER_LENGTH = 20
-BAR_SET_TRAP = False
-BAR_SIGNAL_DEFAULT = None
-
-
-class Log:
-    def __init__(self, filename=TEMP_LOG, stream=sys.stdout):
-        self.terminal = stream
-        self.log = open(filename, "w")
-        fcntl.flock(self.log, fcntl.LOCK_EX | fcntl.LOCK_NB)
-
-    def write(self, message):
-        self.terminal.write(message)
-        try:
-            self.log.write(message)
-        except IOError:
-            fcntl.flock(self.log, fcntl.LOCK_UN)
-            self.log.close()
-            sys.stdout = STDOUT_DEFAULT
-            printAbort(f"Failed to write log file {TEMP_LOG}.")
-
-    def flush(self):
-        pass
-
-    def uninit(self):
-        fcntl.flock(self.log, fcntl.LOCK_UN)
-        self.log.close()
-        sys.stdout = STDOUT_DEFAULT
-
-
-def setupProgressBar():
-    global BAR_CURRENT_LINES
-    curses.setupterm()
-
-    trapDueToInterrupt()
-
-    BAR_CURRENT_LINES = tputLines()
-    lines = BAR_CURRENT_LINES - 1
-    printProgress("\n")
-
-    printProgress(BAR_SAVE_CURSOR)
-    printProgress(f"\033[0;{str(lines)}r")
-
-    printProgress(BAR_RESTORE_CURSOR)
-    printProgress(BAR_MOVE_UP_CURSOR)
-    drawProgressBar(0)
-
-
-def drawProgressBar(percentage):
-    lines = tputLines()
-    if lines != BAR_CURRENT_LINES:
-        setupProgressBar()
-
-    printProgress(BAR_SAVE_CURSOR)
-    printProgress(f"\033[{str(lines)};0f")
-
-    tput()
-    printBar(percentage)
-    printProgress(BAR_RESTORE_CURSOR)
-    time.sleep(0.05)
-
-
-def destroyProgressBar():
-    lines = tputLines()
-    printProgress(BAR_SAVE_CURSOR)
-    printProgress(f"\033[0;{str(lines)}r")
-
-    printProgress(BAR_RESTORE_CURSOR)
-    printProgress(BAR_MOVE_UP_CURSOR)
-
-    clearProgressBar()
-    printProgress("\n\n")
-    if BAR_SET_TRAP:
-        signal.signal(signal.SIGINT, BAR_SIGNAL_DEFAULT)
-
-
-def clearProgressBar():
-    lines = tputLines()
-    printProgress(BAR_SAVE_CURSOR)
-    printProgress(f"\033[{str(lines)};0f")
-
-    tput()
-    printProgress(BAR_RESTORE_CURSOR)
-
-
-def trapDueToInterrupt():
-    global BAR_SET_TRAP
-    global BAR_SIGNAL_DEFAULT
-    BAR_SET_TRAP = True
-    BAR_SIGNAL_DEFAULT = signal.getsignal(signal.SIGINT)
-    signal.signal(signal.SIGINT, clearDueToInterrupt)
-
-
-def clearDueToInterrupt(sign, frame):
-    destroyProgressBar()
-    raise KeyboardInterrupt
-
-
-def printBar(percentage):
-    cols = tputCols()
-    barSize = cols - BAR_PLACEHOLDER_LENGTH
-    color = f"{BAR_FORE_COLOR}{BAR_BACK_COLOR}"
-    defaultColor = f"{BAR_FORE_COLOR_DEFAULT}{BAR_BACK_COLOR_DEFAULT}"
-
-    completeSize = int((barSize * percentage) / 100)
-    remainderSize = barSize - completeSize
-    progressBar = f"[{color}{'#' * int(completeSize)}{defaultColor}{'.' * int(remainderSize)}]"
-    printProgress(f" Progress {percentage:>3}% {progressBar}\r")
-
-
-def printProgress(text):
-    print(text, end="")
-
-
-def tputLines():
-    cmd = executeCommand("tput lines", output=False)
-    out = cmd.stdout.read()
-    return int(out)
-
-
-def tputCols():
-    cmd = executeCommand("tput cols", output=False)
-    out = cmd.stdout.read()
-    return int(out)
-
-
-def tput():
-    print(curses.tparm(curses.tigetstr("el")).decode(), end="")
-
-
-def executeCommand(command, output=True):
-    try:
-        cmd = subprocess.Popen(
-            command,
-            shell=True,
-            executable="/bin/bash",
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            encoding="utf-8",
-        )
-        cmd.wait()
-    except RuntimeError():
-        printAbort(f"Failed to execute command: \"{command}\".")
-    if output:
-        out, err = cmd.communicate()
-        if len(out) != 0:
-            print(out)
-        if len(err) != 0:
-            print(err)
-    return cmd
 
 
 def printAbort(message):
@@ -219,7 +57,7 @@ def printAbort(message):
 
 
 def buildProject(command):
-    cmd = executeCommand(command, output=False)
+    cmd = common.executeCommand(command, output=False)
     out = cmd.stdout.read()
     print(out)
     if cmd.returncode != 0:
@@ -247,7 +85,7 @@ def runTestTask(command):
             x=ALIGN_CMD,
         )
     )
-    cmd = executeCommand(fullCommand, output=False)
+    cmd = common.executeCommand(fullCommand, output=False)
     out = cmd.stdout.read()
     print(out)
     if cmd.returncode == 0:
@@ -302,7 +140,7 @@ def runTestTask(command):
         )
     )
     sys.stdout = STDOUT_DEFAULT
-    drawProgressBar(int(CURRENT_STEP / WHOLE_STEP * 100))
+    common.drawProgressBar(int(CURRENT_STEP / WHOLE_STEP * 100))
     sys.stdout = STDOUT_LOG
 
 
@@ -323,7 +161,7 @@ def parseArgs():
 
     if args.check:
         if "valgrind" in args.check:
-            cmd = executeCommand(f"{COMMAND_DESCRIPTION_CMD} valgrind 2>&1", output=False)
+            cmd = common.executeCommand(f"{COMMAND_DESCRIPTION_CMD} valgrind 2>&1", output=False)
             out = cmd.stdout.read()
             if out.find("valgrind") != -1:
                 global CHECK_SET_VALGRIND
@@ -331,9 +169,13 @@ def parseArgs():
             else:
                 printAbort("There is no valgrind program. Please check it.")
         if "coverage" in args.check:
-            cmd1 = executeCommand(f"{COMMAND_DESCRIPTION_CMD} llvm-profdata-12 2>&1", output=False)
+            cmd1 = common.executeCommand(
+                f"{COMMAND_DESCRIPTION_CMD} llvm-profdata-12 2>&1", output=False
+            )
             out1 = cmd1.stdout.read()
-            cmd2 = executeCommand(f"{COMMAND_DESCRIPTION_CMD} llvm-cov-12 2>&1", output=False)
+            cmd2 = common.executeCommand(
+                f"{COMMAND_DESCRIPTION_CMD} llvm-cov-12 2>&1", output=False
+            )
             out2 = cmd2.stdout.read()
             if out1.find("llvm-profdata-12") != -1 and out2.find("llvm-cov-12") != -1:
                 os.environ["FOO_ENV"] = "CODE_COVERAGE"
@@ -361,28 +203,28 @@ def prepareTest():
     if not os.path.exists(TEMP_PATH):
         os.makedirs(TEMP_PATH)
 
-    setupProgressBar()
-    sys.stdout = Log(TEMP_LOG, sys.stdout)
+    common.setupProgressBar()
+    sys.stdout = common.Log(TEMP_LOG, sys.stdout)
     global STDOUT_LOG
     STDOUT_LOG = sys.stdout
 
 
 def completeTest():
     sys.stdout = STDOUT_DEFAULT
-    destroyProgressBar()
+    common.destroyProgressBar()
     sys.stdout = STDOUT_LOG
     sys.stdout.uninit()
 
     if CHECK_SET_COVERAGE:
-        executeCommand(
+        common.executeCommand(
             f"{CHECK_COVERAGE_PROFDATA_CMD} {TEMP_PATH}/foo_*.profraw -o {TEMP_PATH}/foo.profdata"
         )
-        cmd = executeCommand(
+        cmd = common.executeCommand(
             f"{CHECK_COVERAGE_COV_CMD} {RUN_DIR}{RUN_CMD} -instr-profile={TEMP_PATH}/foo.profdata \
 2>&1",
             output=False,
         )
-        executeCommand(f"rm -rf {TEMP_PATH}/*.profraw")
+        common.executeCommand(f"rm -rf {TEMP_PATH}/*.profraw")
         out = cmd.stdout.read()
         print(out)
         if out.find("error") != -1:
