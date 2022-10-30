e2138b87bb60f3ac771fe57cccc4c4878739751f
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-10-30 12:04:56 +0800

    feat: show backtrace when receive signal SIGSEGV

--- application/include/main.hpp
+++ application/include/main.hpp
@@ -1,23 +1,91 @@
 #pragma once
 
+#include <cxxabi.h>
+#include <dlfcn.h>
+#include <execinfo.h>
+#include <unistd.h>
+#include <csignal>
 #include <filesystem>
 
 [[using gnu: constructor]] static void switchToProjectPath();
 
+static void signalHandler(int sig)
+{
+    void* callstack[128];
+    const int maxFrame = sizeof(callstack) / sizeof(callstack[0]), numOfFrame = backtrace(callstack, maxFrame);
+    char** symbols = backtrace_symbols(callstack, numOfFrame);
+    char buffer[1024];
+    std::ostringstream originalTrace, realTrace;
+
+    for (int i = 1; i < numOfFrame; ++i)
+    {
+        originalTrace << symbols[i] << "\n";
+
+        Dl_info info;
+        if (dladdr(callstack[i], &info) && info.dli_sname)
+        {
+            char* demangle = nullptr;
+            int status = -1;
+            if ('_' == info.dli_sname[0])
+            {
+                demangle = abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
+            }
+            std::snprintf(
+                buffer,
+                sizeof(buffer),
+                "%-3d %*p %s + %zd\n",
+                i,
+                static_cast<int>(2 + sizeof(void*) * 2),
+                callstack[i],
+                (0 == status) ? demangle : ((nullptr == info.dli_sname) ? symbols[i] : info.dli_sname),
+                static_cast<char*>(callstack[i]) - static_cast<char*>(info.dli_saddr));
+            free(demangle); // NOLINT(cppcoreguidelines-no-malloc)
+        }
+        else
+        {
+            std::snprintf(
+                buffer,
+                sizeof(buffer),
+                "%-3d %*p %s\n",
+                i,
+                static_cast<int>(2 + sizeof(void*) * 2),
+                callstack[i],
+                symbols[i]);
+        }
+        realTrace << buffer;
+    }
+    free(symbols); // NOLINT(cppcoreguidelines-no-malloc)
+
+    if (numOfFrame == maxFrame)
+    {
+        realTrace << "\r\n<TRUNCATED...>\n";
+    }
+
+    std::fprintf(
+        stderr,
+        "\r\n<SIGNAL %d>\r\n<ORIGINAL BACKTRACE>\r\n%s\r\n<REAL BACKTRACE>\r\n%s\n",
+        sig,
+        originalTrace.str().c_str(),
+        realTrace.str().c_str());
+    kill(getpid(), SIGKILL);
+}
+
 static void switchToProjectPath()
 {
+    std::signal(SIGSEGV, signalHandler);
+
     const std::filesystem::path absolutePath = std::filesystem::canonical(std::filesystem::path{"/proc/self/exe"});
     const std::size_t pos = absolutePath.string().find("build");
     if (std::string::npos == pos)
     {
-        printf("The build directory does not exist. Please check it.\n");
+        std::fprintf(stderr, "The build directory isn't exist. Please check it.\n");
         std::exit(-1);
     }
     const std::filesystem::path buildPath(std::filesystem::path{absolutePath.string().substr(0, pos)});
 
     if (!buildPath.has_parent_path())
     {
-        printf("The project directory does not exist. Please check it.\n");
+        std::fprintf(stderr, "The project directory isn't exist. Please check it.\n");
         std::exit(-1);
     }
     std::filesystem::current_path(buildPath.parent_path());
