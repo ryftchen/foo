2c12f239d83b40757b5ed8005bc604b64deb89fd
ryftchen <39207065+ryftchen@users.noreply.github.com>
2022-11-02 21:27:45 +0800

    feat: add microsecond in log prefix

--- utility/source/time.cpp
+++ utility/source/time.cpp
@@ -5,19 +5,22 @@ namespace util_time
 std::string getCurrentSystemTime()
 {
     char date[dateLength + 1] = {'\0'};
-    const time_t tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    const auto now = std::chrono::system_clock::now();
+    const auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()) % secToUsec;
+    const time_t tt = std::chrono::system_clock::to_time_t(now);
     const tm* tm = localtime(&tt);
 
     std::snprintf(
         date,
         dateLength + 1,
-        "%u-%02u-%02u %02u:%02u:%02u %s",
-        static_cast<uint32_t>(tm->tm_year) + dateStartYear,
-        static_cast<uint32_t>(tm->tm_mon) + 1,
-        static_cast<uint32_t>(tm->tm_mday),
-        static_cast<uint32_t>(tm->tm_hour),
-        static_cast<uint32_t>(tm->tm_min),
-        static_cast<uint32_t>(tm->tm_sec),
+        "%04u-%02u-%02u %02u:%02u:%02u.%06lu %.3s",
+        tm->tm_year + dateStartYear,
+        tm->tm_mon + 1,
+        tm->tm_mday,
+        tm->tm_hour,
+        tm->tm_min,
+        tm->tm_sec,
+        microseconds.count(),
         tm->tm_zone);
     std::string dateStr(date);
 
@@ -27,7 +30,6 @@ std::string getCurrentSystemTime()
 std::mt19937 getRandomSeedByTime()
 {
     timeval timeSeed{};
-    constexpr uint32_t secToUsec = 1000000;
     gettimeofday(&timeSeed, nullptr);
 
     return std::mt19937(timeSeed.tv_sec * secToUsec + timeSeed.tv_usec);
