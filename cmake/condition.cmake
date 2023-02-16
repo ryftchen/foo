fe982332c8bfeebefcfeca420481c25b4b58c48f
ryftchen <39207065+ryftchen@users.noreply.github.com>
2023-02-16 23:03:26 +0800

    build: use pch to enhance development

--- cmake/condition.cmake
+++ cmake/condition.cmake
@@ -1,8 +1,8 @@
 include_guard()
 
-if(RUNTIME_PRINTING)
-    add_compile_options(-D__RUNTIME_PRINTING)
-endif()
 if(PRECOMPILED_HEADER)
     add_compile_options(-D__PRECOMPILED_HEADER)
+endif()
+if(RUNTIME_PRINTING)
+    add_compile_options(-D__RUNTIME_PRINTING)
 endif()
