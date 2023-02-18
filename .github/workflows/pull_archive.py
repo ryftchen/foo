bd12b65befd5a8d4eaf8676d63bd2f072a0e0b0d
Cecil <39207065+ryftchen@users.noreply.github.com>
2023-02-18 11:48:21 +0800

    chore: enable to pull archive with proxy

--- .github/workflows/pull_archive.py
+++ .github/workflows/pull_archive.py
@@ -3,6 +3,7 @@
 try:
     import json
     import os
+    import argparse
     import subprocess
     import sys
     from datetime import datetime
@@ -14,6 +15,7 @@ GIT_URL = "https://github.com/ryftchen/foo.git"
 ARTIFACT_URL = "https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=5"
 ARTIFACT_NAME = "foo_artifact"
 WEBSITE_DIR = "/var/www/foo_web"
+PROXY_PORT = ""
 
 
 def command(cmd):
@@ -47,6 +49,23 @@ def exception(msg):
     sys.exit(-1)
 
 
+def parseArguments():
+    parser = argparse.ArgumentParser(description="pull archive script")
+    parser.add_argument(
+        "-p",
+        "--port",
+        nargs="?",
+        type=int,
+        choices=range(0, 65535 + 1),
+        help="proxy port",
+        metavar="[0-65535]",
+    )
+    args = parser.parse_args()
+    if args.port is not None:
+        global PROXY_PORT
+        PROXY_PORT = args.port
+
+
 def downloadArtifact():
     filePath = os.path.split(os.path.realpath(__file__))[0]
     os.chdir(filePath.replace(filePath[filePath.index(".github") :], ''))
@@ -60,7 +79,7 @@ def downloadArtifact():
     if localCommitId != remoteCommitId:
         command("git pull origin master")
     elif os.path.exists(f"{WEBSITE_DIR}/browser") and os.path.exists(f"{WEBSITE_DIR}/doxygen"):
-        exception("No change in git project.")
+        exception("No commit change.")
 
     try:
         response = requests.get(ARTIFACT_URL, timeout=60)
@@ -76,11 +95,13 @@ def downloadArtifact():
         response = requests.get(downloadUrl, timeout=60, allow_redirects=False)
         response.raise_for_status()
         locationUrl = response.headers["location"]
-        proxy = {
-            # "http": "http://localhost:80",
-            # "https": "https://localhost:80",
-            # "ftp": "ftp://localhost:80"
-        }
+        proxy = {}
+        if PROXY_PORT:
+            proxy = {
+                "http": f"http://localhost:{PROXY_PORT}",
+                "https": f"https://localhost:{PROXY_PORT}",
+                "ftp": f"ftp://localhost:{PROXY_PORT}",
+            }
         response = requests.get(locationUrl, timeout=60, proxies=proxy)
         response.raise_for_status()
         with open(f"{WEBSITE_DIR}/{ARTIFACT_NAME}.zip", "wb") as outputFile:
@@ -97,7 +118,7 @@ def downloadArtifact():
         exception(f"The {ARTIFACT_NAME}.zip file in the {WEBSITE_DIR} folder is corrupted.")
 
 
-def archiveDocument():
+def updateDocument():
     command(f"rm -rf {WEBSITE_DIR}/browser {WEBSITE_DIR}/doxygen")
     command(f"unzip {WEBSITE_DIR}/{ARTIFACT_NAME}.zip -d {WEBSITE_DIR}")
     command(f"tar -jxvf {WEBSITE_DIR}/foo_browser_*.tar.bz2 -C {WEBSITE_DIR} >/dev/null")
@@ -105,10 +126,10 @@ def archiveDocument():
     command(f"rm -rf {WEBSITE_DIR}/*.zip {WEBSITE_DIR}/*.tar.bz2")
 
 
-def pull():
+def pullArchive():
     print(
         f'\r\n{datetime.strftime(datetime.now(), "%b %d %H:%M:%S")} \
->>>>>>>>>>>>>>>>>>>>>>>>> ARCHIVE <<<<<<<<<<<<<<<<<<<<<<<<<'
+>>>>>>>>>>>>>>>>>>>>>>>>> PULL ARCHIVE <<<<<<<<<<<<<<<<<<<<<<<<<'
     )
     env = os.getenv("FOO_ENV")
     if env:
@@ -118,8 +139,9 @@ def pull():
         exception("Please export the environment variable FOO_ENV.")
 
     downloadArtifact()
-    archiveDocument()
+    updateDocument()
 
 
 if __name__ == "__main__":
-    pull()
+    parseArguments()
+    pullArchive()
