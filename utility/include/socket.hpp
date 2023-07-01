9fd5481941c44f04edd0fa15900db783fd252b8e
ryftchen <39207065+ryftchen@users.noreply.github.com>
2023-07-01 15:52:15 +0800

    fix: resource not released in destructor

--- utility/include/socket.hpp
+++ utility/include/socket.hpp
@@ -52,7 +52,7 @@ protected:
     //! @param socketId -  socket id
     explicit Socket(const Type socketType = tcp, const int socketId = -1);
     //! @brief Destroy the Socket object.
-    virtual ~Socket() = default;
+    virtual ~Socket();
 
     //! @brief Get the ip address from transport information.
     //! @param addr - transport information
