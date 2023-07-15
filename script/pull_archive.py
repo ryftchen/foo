a9ad34986c037e9015dacc9848a28802d2ab23c5
ryftchen <39207065+ryftchen@users.noreply.github.com>
2023-07-15 13:18:14 +0800

    chore: move pull_archive.py to script folder

--- script/pull_archive.py
+++ script/pull_archive.py
@@ -5,44 +5,23 @@ try:
     import json
     import os
     import requests
-    import subprocess
     import sys
     import traceback
     from datetime import datetime
+    import common
 except ImportError as err:
     raise ImportError(err) from err
 
-GIT_URL = "https://github.com/ryftchen/foo.git"
+GITHUB_URL = "https://github.com/ryftchen/foo.git"
 ARTIFACT_URL = "https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=5"
-ARTIFACT_NAME = "foo_artifact"
+ARTIFACT_FILE = "foo_artifact"
 WEBSITE_DIR = "/var/www/foo_web"
 PROXY_PORT = ""
 
 
-def command(cmd):
-    print(f"{datetime.strftime(datetime.now(), '%b %d %H:%M:%S')} command: {cmd}")
-    try:
-        process = subprocess.run(
-            cmd,
-            shell=True,
-            executable="/bin/bash",
-            universal_newlines=True,
-            capture_output=True,
-            check=True,
-            encoding="utf-8",
-            timeout=300,
-        )
-        if process.stdout:
-            print(process.stdout.strip())
-        if process.stderr:
-            print(process.stderr.strip())
-        return process.stdout.strip(), process.stderr.strip()
-    except subprocess.CalledProcessError as error:
-        print(error)
-        sys.exit(-1)
-    except subprocess.TimeoutExpired as error:
-        print(error)
-        sys.exit(-1)
+def execute(cmd):
+    print(f"{datetime.strftime(datetime.now(), '%b %d %H:%M:%S')} execute: {cmd}")
+    return common.execute_command(cmd)
 
 
 def abort(msg):
@@ -51,17 +30,14 @@ def abort(msg):
 
 
 def download_artifact():
-    file_path = os.path.split(os.path.realpath(__file__))[0]
-    os.chdir(file_path.replace(file_path[file_path.index(".github") :], ""))
-
     if not os.path.exists(WEBSITE_DIR):
         abort("Please create a foo_web folder in the /var/www directory.")
-    local_commit_id, _ = command("git rev-parse HEAD")
-    remote_commit_id, _ = command(f"git ls-remote {GIT_URL} refs/heads/master | cut -f 1")
+    local_commit_id, _, _ = execute("git rev-parse HEAD")
+    remote_commit_id, _, _ = execute(f"git ls-remote {GITHUB_URL} refs/heads/master | cut -f 1")
     if not remote_commit_id:
         abort("Failed to get the latest commit id.")
     if local_commit_id != remote_commit_id:
-        command("git pull origin master")
+        execute("git pull origin master")
     elif os.path.exists(f"{WEBSITE_DIR}/browser") and os.path.exists(f"{WEBSITE_DIR}/doxygen"):
         abort("No commit change.")
 
@@ -72,7 +48,7 @@ def download_artifact():
         download_url = ""
         json_info = json.loads(response.text)
         for index in range(json_info["total_count"]):
-            if json_info["artifacts"][index]["name"] == ARTIFACT_NAME:
+            if json_info["artifacts"][index]["name"] == ARTIFACT_FILE:
                 download_url = json_info["artifacts"][index]["archive_download_url"]
                 break
 
@@ -88,26 +64,26 @@ def download_artifact():
             }
         response = requests.get(location_url, timeout=60, proxies=proxy)
         response.raise_for_status()
-        with open(f"{WEBSITE_DIR}/{ARTIFACT_NAME}.zip", "wb") as output_file:
+        with open(f"{WEBSITE_DIR}/{ARTIFACT_FILE}.zip", "wb") as output_file:
             output_file.write(response.content)
     except requests.exceptions.HTTPError as error:
-        command(f"rm -rf {WEBSITE_DIR}/{ARTIFACT_NAME}.zip")
-        command(f"git reset --hard {local_commit_id}")
+        execute(f"rm -rf {WEBSITE_DIR}/{ARTIFACT_FILE}.zip")
+        execute(f"git reset --hard {local_commit_id}")
         abort(error)
 
-    validation, _ = command(f"zip -T {WEBSITE_DIR}/{ARTIFACT_NAME}.zip")
+    validation, _, _ = execute(f"zip -T {WEBSITE_DIR}/{ARTIFACT_FILE}.zip")
     if "zip error" in validation:
-        command(f"rm -rf {WEBSITE_DIR}/{ARTIFACT_NAME}.zip")
-        command(f"git reset --hard {local_commit_id}")
-        abort(f"The {ARTIFACT_NAME}.zip file in the {WEBSITE_DIR} folder is corrupted.")
+        execute(f"rm -rf {WEBSITE_DIR}/{ARTIFACT_FILE}.zip")
+        execute(f"git reset --hard {local_commit_id}")
+        abort(f"The {ARTIFACT_FILE}.zip file in the {WEBSITE_DIR} folder is corrupted.")
 
 
 def update_document():
-    command(f"rm -rf {WEBSITE_DIR}/browser {WEBSITE_DIR}/doxygen")
-    command(f"unzip {WEBSITE_DIR}/{ARTIFACT_NAME}.zip -d {WEBSITE_DIR}")
-    command(f"tar -jxvf {WEBSITE_DIR}/foo_browser_*.tar.bz2 -C {WEBSITE_DIR} >/dev/null")
-    command(f"tar -jxvf {WEBSITE_DIR}/foo_doxygen_*.tar.bz2 -C {WEBSITE_DIR} >/dev/null")
-    command(f"rm -rf {WEBSITE_DIR}/*.zip {WEBSITE_DIR}/*.tar.bz2")
+    execute(f"rm -rf {WEBSITE_DIR}/browser {WEBSITE_DIR}/doxygen")
+    execute(f"unzip {WEBSITE_DIR}/{ARTIFACT_FILE}.zip -d {WEBSITE_DIR}")
+    execute(f"tar -jxvf {WEBSITE_DIR}/foo_browser_*.tar.bz2 -C {WEBSITE_DIR} >/dev/null")
+    execute(f"tar -jxvf {WEBSITE_DIR}/foo_doxygen_*.tar.bz2 -C {WEBSITE_DIR} >/dev/null")
+    execute(f"rm -rf {WEBSITE_DIR}/*.zip {WEBSITE_DIR}/*.tar.bz2")
 
 
 def pull_archive():
@@ -136,6 +112,9 @@ def pull_archive():
             abort("The environment variable FOO_ENV must be foo_doc.")
     else:
         abort("Please export the environment variable FOO_ENV.")
+
+    file_path = os.path.split(os.path.realpath(__file__))[0]
+    os.chdir(file_path.replace(file_path[file_path.index("script") :], ""))
 
     download_artifact()
     update_document()
