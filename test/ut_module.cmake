9c4825c630fe57e568a139e79792a5c462168148
ryftchen <39207065+ryftchen@users.noreply.github.com>
2023-08-06 13:02:12 +0800

    build: simplify unit test dependencies

--- test/ut_module.cmake
+++ test/ut_module.cmake
@@ -1,9 +1,8 @@
 include_guard()
 
-set(MODULE_NAME_1 test_algorithm)
-file(GLOB TEST_ALGORITHM_LIST ${CMAKE_SOURCE_DIR}/unit/test_algorithm.cpp)
-aux_source_directory(${CMAKE_SOURCE_DIR}/../algorithm/source TEST_ALGORITHM_LIST)
-add_library(${MODULE_NAME_1} STATIC ${TEST_ALGORITHM_LIST})
+set(MODULE_NAME_1 algorithm)
+aux_source_directory(${CMAKE_SOURCE_DIR}/../algorithm/source ALGORITHM_LIST)
+add_library(${MODULE_NAME_1} STATIC ${ALGORITHM_LIST})
 add_library(foo_test::${MODULE_NAME_1} ALIAS ${MODULE_NAME_1})
 target_include_directories(
     ${MODULE_NAME_1}
@@ -11,10 +10,9 @@ target_include_directories(
     PRIVATE ${CMAKE_SOURCE_DIR}/../algorithm/source
 )
 
-set(MODULE_NAME_2 test_data_structure)
-file(GLOB TEST_DATA_STRUCTURE_LIST ${CMAKE_SOURCE_DIR}/unit/test_data_structure.cpp)
-aux_source_directory(${CMAKE_SOURCE_DIR}/../data_structure/source TEST_DATA_STRUCTURE_LIST)
-add_library(${MODULE_NAME_2} STATIC ${TEST_DATA_STRUCTURE_LIST})
+set(MODULE_NAME_2 data_structure)
+aux_source_directory(${CMAKE_SOURCE_DIR}/../data_structure/source DATA_STRUCTURE_LIST)
+add_library(${MODULE_NAME_2} STATIC ${DATA_STRUCTURE_LIST})
 add_library(foo_test::${MODULE_NAME_2} ALIAS ${MODULE_NAME_2})
 target_include_directories(
     ${MODULE_NAME_2}
@@ -22,10 +20,9 @@ target_include_directories(
     PRIVATE ${CMAKE_SOURCE_DIR}/../data_structure/source
 )
 
-set(MODULE_NAME_3 test_design_pattern)
-file(GLOB TEST_DESIGN_PATTERN_LIST ${CMAKE_SOURCE_DIR}/unit/test_design_pattern.cpp)
-aux_source_directory(${CMAKE_SOURCE_DIR}/../design_pattern/source TEST_DESIGN_PATTERN_LIST)
-add_library(${MODULE_NAME_3} STATIC ${TEST_DESIGN_PATTERN_LIST})
+set(MODULE_NAME_3 design_pattern)
+aux_source_directory(${CMAKE_SOURCE_DIR}/../design_pattern/source DESIGN_PATTERN_LIST)
+add_library(${MODULE_NAME_3} STATIC ${DESIGN_PATTERN_LIST})
 add_library(foo_test::${MODULE_NAME_3} ALIAS ${MODULE_NAME_3})
 target_include_directories(
     ${MODULE_NAME_3}
@@ -33,10 +30,9 @@ target_include_directories(
     PRIVATE ${CMAKE_SOURCE_DIR}/../design_pattern/source
 )
 
-set(MODULE_NAME_4 test_numeric)
-file(GLOB TEST_NUMERIC_LIST ${CMAKE_SOURCE_DIR}/unit/test_numeric.cpp)
-aux_source_directory(${CMAKE_SOURCE_DIR}/../numeric/source TEST_NUMERIC_LIST)
-add_library(${MODULE_NAME_4} STATIC ${TEST_NUMERIC_LIST})
+set(MODULE_NAME_4 numeric)
+aux_source_directory(${CMAKE_SOURCE_DIR}/../numeric/source NUMERIC_LIST)
+add_library(${MODULE_NAME_4} STATIC ${NUMERIC_LIST})
 add_library(foo_test::${MODULE_NAME_4} ALIAS ${MODULE_NAME_4})
 target_include_directories(
     ${MODULE_NAME_4}
