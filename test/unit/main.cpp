39dc2d819c9f731d97fa612e3a4f7dd5e533f10f
ryftchen <39207065+ryftchen@users.noreply.github.com>
2023-08-23 19:57:35 +0800

    style: improve readability

--- test/unit/main.cpp
+++ test/unit/main.cpp
@@ -6,13 +6,26 @@
 
 #include <gtest/gtest.h>
 
+//! @brief The test module.
+namespace test
+{
+//! @brief The run function.
+//! @param argc - argument count
+//! @param argv - argument vector
+//! @return the argument to the implicit call to exit()
+static int run(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return ::RUN_ALL_TESTS();
+}
+} // namespace test
+
 //! @brief The main function of the unit test.
 //! @param argc - argument count
 //! @param argv - argument vector
 //! @return the argument to the implicit call to exit()
 int main(int argc, char* argv[])
 {
-    ::testing::InitGoogleTest(&argc, argv);
-
-    return ::RUN_ALL_TESTS();
+    return test::run(argc, argv);
 }
