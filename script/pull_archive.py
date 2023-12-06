0b46708f7924f23fbe7cdf609ce1577edca1eb07
ryftchen <39207065+ryftchen@users.noreply.github.com>
2023-12-06 21:46:09 +0800

    style: improve readability

--- script/pull_archive.py
+++ script/pull_archive.py
@@ -31,14 +31,14 @@ class Documentation:
         env = os.getenv("FOO_ENV")
         if env is not None:
             if env != "foo_doc":
-                abort("The environment variable FOO_ENV must be foo_doc.")
+                interrupt("The environment variable FOO_ENV must be foo_doc.")
         else:
-            abort("Please export the environment variable FOO_ENV.")
+            interrupt("Please export the environment variable FOO_ENV.")
 
     def pull_archive(self):
         script_path = os.path.split(os.path.realpath(__file__))[0]
         if not fnmatch.fnmatch(script_path, "*foo/script"):
-            abort("Illegal path to current script.")
+            interrupt("Illegal path to current script.")
         self.project_path = os.path.dirname(script_path)
 
         parser = argparse.ArgumentParser(description="pull archive script")
@@ -58,7 +58,7 @@ class Documentation:
         sys.stdout = self.logger
         print(
             f"\r\n[ {datetime.strftime(datetime.now(), '%b %d %H:%M:%S')} ] \
->>>>>>>>>>>>>>>>>>>>>>> PULL ARCHIVE <<<<<<<<<<<<<<<<<<<<<<<"
+####################### PULL ARCHIVE #######################"
         )
         self.download_artifact()
         self.update_document()
@@ -67,17 +67,17 @@ class Documentation:
 
     def download_artifact(self):
         if not os.path.exists(self.website_dir):
-            abort("Please manually create a foo_doc folder in the /var/www directory.")
+            interrupt("Please manually create a foo_doc folder in the /var/www directory.")
         local_commit_id, _, _ = execute(f"git -C {self.project_path} rev-parse HEAD")
         remote_commit_id, _, _ = execute(
             f"git -C {self.project_path} ls-remote {self.github_url} refs/heads/master | cut -f 1"
         )
         if not remote_commit_id:
-            abort("Failed to get the latest commit id.")
+            interrupt("Failed to get the latest commit id.")
         if local_commit_id != remote_commit_id:
             execute(f"git -C {self.project_path} pull origin master")
         elif os.path.exists(f"{self.website_dir}/browser") and os.path.exists(f"{self.website_dir}/doxygen"):
-            abort("No commit change.")
+            interrupt("No commit change.")
 
         try:
             response = requests.get(self.artifact_url, timeout=60)
@@ -107,13 +107,13 @@ class Documentation:
         except requests.exceptions.HTTPError as error:
             execute(f"rm -rf {self.website_dir}/{self.artifact_file}.zip")
             execute(f"git -C {self.project_path} reset --hard {local_commit_id}")
-            abort(error)
+            interrupt(error)
 
         validation, _, _ = execute(f"zip -T {self.website_dir}/{self.artifact_file}.zip")
         if "zip error" in validation:
             execute(f"rm -rf {self.website_dir}/{self.artifact_file}.zip")
             execute(f"git -C {self.project_path} reset --hard {local_commit_id}")
-            abort(f"The {self.artifact_file}.zip file in the {self.website_dir} folder is corrupted.")
+            interrupt(f"The {self.artifact_file}.zip file in the {self.website_dir} folder is corrupted.")
 
     def update_document(self):
         execute(f"rm -rf {self.website_dir}/browser {self.website_dir}/doxygen")
@@ -128,8 +128,8 @@ def execute(cmd):
     return common.execute_command(cmd)
 
 
-def abort(msg):
-    print(f"[ {datetime.strftime(datetime.now(), '%b %d %H:%M:%S')} ] ABORT: {msg}")
+def interrupt(msg):
+    print(f"[ {datetime.strftime(datetime.now(), '%b %d %H:%M:%S')} ] INTERRUPT: {msg}")
     sys.exit(1)
 
 
@@ -137,4 +137,4 @@ if __name__ == "__main__":
     try:
         Documentation().pull_archive()
     except Exception:  # pylint: disable=broad-except
-        abort(traceback.format_exc())
+        interrupt(traceback.format_exc())
