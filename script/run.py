dd2b00c39ac261ee74defb039e22ada68aab0f10
ryftchen <39207065+ryftchen@users.noreply.github.com>
2024-01-27 21:43:51 +0800

    style: improve readability

--- script/run.py
+++ script/run.py
@@ -35,7 +35,7 @@ class Task:
             r"batch ./script/console_batch.txt",
             r"refresh",
             r"reconnect",
-            r'''bash "ps -ewwo size,pid,user,command --sort -size | awk '{ hr=\$1/1024 ; printf(\"%6.2fMB \", hr) } { for (x=4 ; x<=NF ; x++) { printf(\"%s \", \$x) } print \"\" }' | cut -d '' -f 2 | sed '1d'"''',  # pylint: disable=line-too-long
+            r'''bash "ps -eww -o size,pid,user,command --sort -size | awk '{ hr=\$1/1024 ; printf(\"%6.2fMB \", hr) } { for (x=4 ; x<=NF ; x++) { printf(\"%s \", \$x) } print \"\" }' | cut -d '' -f 2 | sed '1d'"''',  # pylint: disable=line-too-long
             r"journal",
             r"monitor",
         ],
