c7b647f617169c6f1ec39d034a10e13f9d109f34
ryftchen <39207065+ryftchen@users.noreply.github.com>
2024-05-31 01:31:06 +0800

    build: add dependency checking for libstdc++

--- script/build.sh
+++ script/build.sh
@@ -740,11 +740,14 @@ function set_compile_condition()
     local tmpfs_subfolder=$1 tmpfs_size=$2
 
     export CC=/usr/bin/clang-16 CXX=/usr/bin/clang++-16
-    local gcc_processor gxx_processor
-    gcc_processor=$(gcc -dumpmachine)
-    gxx_processor=$(g++ -dumpmachine)
-    export C_INCLUDE_PATH=/usr/include:/usr/lib/gcc/${gcc_processor}/12/include \
-        CPLUS_INCLUDE_PATH=/usr/include/c++/12:/usr/include/${gxx_processor}/c++/12
+    if command -v gcc >/dev/null 2>&1 && command -v g++ >/dev/null 2>&1; then
+        local gcc_processor gxx_processor
+        gcc_processor=$(gcc -dumpmachine)
+        gxx_processor=$(g++ -dumpmachine)
+        export C_INCLUDE_PATH=/usr/include:/usr/lib/gcc/${gcc_processor}/12/include \
+            CPLUS_INCLUDE_PATH=/usr/include/c++/12:/usr/include/${gxx_processor}/c++/12
+    fi
+
     if [[ -f ./${FOLDER[scr]}/.env ]]; then
         # shellcheck source=/dev/null
         source "./${FOLDER[scr]}/.env"
