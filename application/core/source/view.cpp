562b8702f1941d554f3ca0fd302af6bb38c67bba
ryftchen <39207065+ryftchen@users.noreply.github.com>
2024-07-28 23:44:32 +0800

    fix: miss timeout operation for eu-stack

--- application/core/source/view.cpp
+++ application/core/source/view.cpp
@@ -854,7 +854,8 @@ std::string View::getStatusReports(const std::uint16_t frame)
                 std::snprintf(
                     cmd + usedLen,
                     totalLen - usedLen,
-                    "&& echo 'Stack:' && (eu-stack -1v -n %d -p %d 2>&1 | grep '#' || exit 0) ; fi",
+                    "&& echo 'Stack:' && (timeout --preserve-status --signal=2 0.5 eu-stack -1v -n %d -p %d 2>&1 "
+                    "| grep '#' || exit 0) ; fi",
                     frame,
                     tid);
             }
