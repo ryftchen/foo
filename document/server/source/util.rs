e1944ddbddd036296dbb0afd576a0d90fee82a92
ryftchen <39207065+ryftchen@users.noreply.github.com>
2025-04-14 19:05:04 +0800

    style: improve readability

--- document/server/source/util.rs
+++ document/server/source/util.rs
@@ -2,6 +2,13 @@ use std::ffi::OsStr;
 use std::path::Path;
 
 #[macro_export]
+macro_rules! trace {
+    ($($msg:tt)*) => {{
+        eprintln!("[ {} ] {}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.6f"), format!($($msg)*));
+    }};
+}
+
+#[macro_export]
 macro_rules! die {
     ($($msg:tt)*) => {
         panic!("{}: {}", util::try_get_exec_name().unwrap(), format!($($msg)*))
@@ -26,14 +33,14 @@ macro_rules! abs_path {
 pub fn try_get_exec_name() -> Option<String> {
     std::env::current_exe()
         .ok()
-        .and_then(|pb| pb.file_name().map(|s| s.to_os_string()))
-        .and_then(|s| s.into_string().ok())
+        .and_then(|path| path.file_name().map(|name| name.to_os_string()))
+        .and_then(|name| name.into_string().ok())
 }
 
 #[inline(always)]
 pub fn try_get_abs_path(path: &str) -> Option<String> {
     shellexpand::full(path)
         .ok()
-        .and_then(|x| Path::new(OsStr::new(x.as_ref())).canonicalize().ok())
-        .and_then(|p| p.into_os_string().into_string().ok())
+        .and_then(|expanded| Path::new(OsStr::new(expanded.as_ref())).canonicalize().ok())
+        .and_then(|path| path.into_os_string().into_string().ok())
 }
