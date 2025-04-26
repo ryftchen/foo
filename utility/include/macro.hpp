78eca4231ecd9a9ae30b09585f45baf5bc1163a9
ryftchen <39207065+ryftchen@users.noreply.github.com>
2025-04-26 17:23:59 +0800

    refactor: separate the public macro definitions

--- utility/include/macro.hpp
+++ utility/include/macro.hpp
@@ -8,6 +8,7 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <utility>
 
 //! @brief Do stringification.
 #define MACRO_STRINGIFY(x) MACRO_STRINGIFY_OP(x)
@@ -28,9 +29,12 @@
     while (0)
 
 //! @brief Always assert.
-#define MACRO_ASSERT(cond)                          \
-    (static_cast<bool>(cond) ? static_cast<void>(0) \
-                             : utility::macro::assertFailure(#cond, __FILE__, __LINE__, __func__))
+#define MACRO_ASSERT(expr) \
+    (static_cast<bool>(expr) ? static_cast<void>(0) : utility::macro::assertFail(#expr, __FILE__, __LINE__, __func__))
+
+//! @brief Defer process.
+#define MACRO_DEFER(lambda) \
+    [[maybe_unused]] const auto MACRO_CONCAT(_deferGuard, __LINE__) = utility::macro::makeDeferral(lambda)
 
 //! @brief The utility module.
 namespace utility // NOLINT(modernize-concat-nested-namespaces)
@@ -42,22 +46,61 @@ extern const char* version() noexcept;
 
 //! @brief Safely ignore all provided arguments.
 //! @tparam Args - type of all provided arguments
-//! @param args - all provided arguments
 template <typename... Args>
-inline constexpr void ignoreAll(Args&&... args)
+inline constexpr void ignoreAll(Args&&... /*args*/) noexcept
 {
-    (static_cast<void>(args), ...);
 }
 
 //! @brief Assertion failed.
-//! @param expr - condition expression
+//! @param assertion - assertion expression
 //! @param file - file path
 //! @param line - line number
-//! @param func - function name
-inline void assertFailure(const char* const expr, const char* const file, const int line, const char* const func)
+//! @param function - function name
+[[noreturn]] inline void assertFail(
+    const char* const assertion, const char* const file, const unsigned int line, const char* const function) noexcept
 {
-    std::fprintf(::stderr, "%s:%d: %s: Assertion '%s' failed.\n", file, line, func, expr);
+    std::fprintf(::stderr, "%s:%d: %s: Assertion `%s` failed.\n", file, line, function, assertion);
     std::abort();
+}
+
+//! @brief Defer the execution of the callable until it is out of scope.
+//! @tparam T - type of deferred callable
+template <typename T>
+class Defer
+{
+public:
+    //! @brief Construct a new Defer object.
+    //! @tparam U - type of deferred callable
+    //! @param deferred - deferred callable
+    template <typename U>
+    explicit Defer(U&& deferred) : deferred{std::forward<U>(deferred)}
+    {
+    }
+    //! @brief Destroy the Defer object.
+    ~Defer() { deferred(); }
+    //! @brief Construct a new Defer object.
+    Defer(const Defer&) = delete;
+    //! @brief Construct a new Defer object.
+    Defer(Defer&&) = delete;
+    //! @brief The operator (=) overloading of Defer class.
+    //! @return reference of the Defer object
+    Defer& operator=(const Defer&) = delete;
+    //! @brief The operator (=) overloading of Defer class.
+    //! @return reference of the Defer object
+    Defer& operator=(Defer&&) = delete;
+
+private:
+    //! @brief The deferred callable.
+    T deferred{};
+};
+//! @brief Create a deferral from a callable.
+//! @tparam T - type of deferred callable
+//! @param deferred - deferred callable
+//! @return deferral
+template <typename T>
+inline constexpr auto makeDeferral(T&& deferred) noexcept
+{
+    return Defer<std::decay_t<T>>(std::forward<T>(deferred));
 }
 } // namespace macro
 } // namespace utility
