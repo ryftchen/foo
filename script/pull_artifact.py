569a858753f29662fc4308e2d8ee494167ef9534
ryftchen <39207065+ryftchen@users.noreply.github.com>
2025-05-28 21:33:59 +0800

    style: rename variables and functions for internal use

--- script/pull_artifact.py
+++ script/pull_artifact.py
@@ -19,31 +19,41 @@ except ImportError as err:
     raise ImportError(err) from err
 
 
-class Schedule:
-    repo_url = "https://github.com/ryftchen/foo.git"
-    api_url = "https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=5"
-    artifact_name = "foo_artifact"
-    target_dir = "/var/www/foo_doc"
-    netrc_file = "~/.netrc"
+class Schedule:  # pylint: disable=too-few-public-methods
+    _repo_url = "https://github.com/ryftchen/foo.git"
+    _api_url = "https://api.github.com/repos/ryftchen/foo/actions/artifacts?per_page=5"
+    _artifact_name = "foo_artifact"
+    _target_dir = "/var/www/foo_doc"
+    _netrc_file = "~/.netrc"
 
-    def __init__(self, logger):
-        self.logger = logger
-        self.forced_pull = False
-        self.proxy_port = None
+    def __init__(self, logger: logging.Logger):
+        self._logger = logger
+        self._forced_pull = False
+        self._proxy_port = None
 
         env = os.getenv("FOO_ENV")
         if env is not None:
             if env != "foo_doc":
-                self.abort("The environment variable FOO_ENV must be foo_doc.")
+                self._abort("The environment variable FOO_ENV must be foo_doc.")
         else:
-            self.abort("Please export the environment variable FOO_ENV.")
-        script_path = os.path.split(os.path.realpath(__file__))[0]
-        if not fnmatch.fnmatch(script_path, "*foo/script"):
-            self.abort("Illegal path to current script.")
-        self.project_path = os.path.dirname(script_path)
+            self._abort("Please export the environment variable FOO_ENV.")
+        self_path = os.path.split(os.path.realpath(__file__))[0]
+        if not fnmatch.fnmatch(self_path, "*foo/script"):
+            self._abort("Illegal path to current script.")
+        self._project_path = os.path.dirname(self_path)
 
-    def configure_from_cli(self):
-        def check_port_range(value):
+    def pull_artifact(self):
+        self._configure_from_cli()
+
+        self._notice(">>>>>>>>>>>>>>>> PULL ARTIFACT >>>>>>>>>>>>>>>>")
+        if not os.path.exists(self._target_dir):
+            self._abort(f"Please create a {self._target_dir} folder for storing pages.")
+        self._download_artifact()
+        self._update_document()
+        self._notice("<<<<<<<<<<<<<<<< PULL ARTIFACT <<<<<<<<<<<<<<<<")
+
+    def _configure_from_cli(self):
+        def _check_port_range(value: int):
             value = int(value)
             if 0 <= value <= 65535:
                 return value
@@ -55,110 +65,100 @@ class Schedule:
             "-p",
             "--port",
             nargs="?",
-            type=check_port_range,
+            type=_check_port_range,
             help="proxy port",
             metavar="[0-65535]",
         )
 
         args = parser.parse_args()
         if args.force:
-            self.forced_pull = True
+            self._forced_pull = True
         if args.port is not None:
-            self.proxy_port = args.port
+            self._proxy_port = args.port
 
-    def pull_artifact(self):
-        self.configure_from_cli()
-
-        self.notice(">>>>>>>>>>>>>>>> PULL ARTIFACT >>>>>>>>>>>>>>>>")
-        if not os.path.exists(self.target_dir):
-            self.abort(f"Please create a {self.target_dir} folder for storing pages.")
-        self.download_artifact()
-        self.update_document()
-        self.notice("<<<<<<<<<<<<<<<< PULL ARTIFACT <<<<<<<<<<<<<<<<")
-
-    def download_artifact(self):
-        self.notice("############## DOWNLOAD ARTIFACT ##############")
-        local_commit_id, _, _ = self.executor(f"git -C {self.project_path} rev-parse HEAD")
-        remote_commit_id, _, _ = self.executor(
-            f"git -C {self.project_path} ls-remote {self.repo_url} refs/heads/master | cut -f 1"
+    def _download_artifact(self):
+        self._notice("############## DOWNLOAD ARTIFACT ##############")
+        local_commit_id, _, _ = self._executor(f"git -C {self._project_path} rev-parse HEAD")
+        remote_commit_id, _, _ = self._executor(
+            f"git -C {self._project_path} ls-remote {self._repo_url} refs/heads/master | cut -f 1"
         )
         if not remote_commit_id:
-            self.abort("Could not get the latest commit id.")
+            self._abort("Could not get the latest commit id.")
         if local_commit_id != remote_commit_id:
-            self.executor(f"git -C {self.project_path} pull origin master")
+            self._executor(f"git -C {self._project_path} pull origin master")
         elif (
-            not self.forced_pull
-            and os.path.exists(f"{self.target_dir}/doxygen")
-            and os.path.exists(f"{self.target_dir}/browser")
+            not self._forced_pull
+            and os.path.exists(f"{self._target_dir}/doxygen")
+            and os.path.exists(f"{self._target_dir}/browser")
         ):
-            self.abort("No commit change.")
+            self._abort("No commit change.")
 
         try:
-            if self.proxy_port is not None:
+            if self._proxy_port is not None:
                 proxy = {
-                    "http": f"http://localhost:{self.proxy_port}",
-                    "https": f"https://localhost:{self.proxy_port}",
-                    "ftp": f"ftp://localhost:{self.proxy_port}",
+                    "http": f"http://localhost:{self._proxy_port}",
+                    "https": f"https://localhost:{self._proxy_port}",
+                    "ftp": f"ftp://localhost:{self._proxy_port}",
                 }
                 handler = urllib.request.ProxyHandler(proxy)
                 opener = urllib.request.build_opener(handler)
                 urllib.request.install_opener(opener)
 
             headers = {}
-            netrc_info = netrc.netrc(os.path.expanduser(self.netrc_file))
-            _, _, access_token = netrc_info.authenticators(urllib.parse.urlparse(self.api_url).hostname)
+            netrc_info = netrc.netrc(os.path.expanduser(self._netrc_file))
+            _, _, access_token = netrc_info.authenticators(urllib.parse.urlparse(self._api_url).hostname)
             if access_token:
                 headers = {"Authorization": f"token {access_token}", "User-Agent": "ryftchen/foo"}
 
             download_url = ""
-            request = urllib.request.Request(self.api_url, headers=headers)
+            request = urllib.request.Request(self._api_url, headers=headers)
             with urllib.request.urlopen(request, timeout=60) as response:
                 if response.status != http.HTTPStatus.OK:
                     raise urllib.error.HTTPError(
-                        self.api_url, response.status, "HTTP request failed.", response.headers, None
+                        self._api_url, response.status, "HTTP request failed.", response.headers, None
                     )
                 json_detail = json.loads(response.read().decode("utf-8"))
                 for index in range(json_detail["total_count"]):
-                    if json_detail["artifacts"][index]["name"] == self.artifact_name:
+                    if json_detail["artifacts"][index]["name"] == self._artifact_name:
                         download_url = json_detail["artifacts"][index]["archive_download_url"]
                         break
 
-            redirect_location = self.get_redirect_location(download_url, headers)
+            redirect_location = self._get_redirect_location(download_url, headers)
             request = urllib.request.Request(redirect_location)
             with urllib.request.urlopen(request, timeout=60) as response:
                 if response.status != http.HTTPStatus.OK:
                     raise urllib.error.HTTPError(
                         redirect_location, response.status, "File download failed.", response.headers, None
                     )
-                with open(f"{self.target_dir}/{self.artifact_name}.zip", "wb") as output_file:
+                with open(f"{self._target_dir}/{self._artifact_name}.zip", "wb") as output_file:
                     output_file.write(response.read())
 
-            with zipfile.ZipFile(f"{self.target_dir}/{self.artifact_name}.zip", "r") as zip_file:
+            with zipfile.ZipFile(f"{self._target_dir}/{self._artifact_name}.zip", "r") as zip_file:
                 if zip_file.testzip() is not None:
                     raise zipfile.BadZipFile("Corrupted zip file.")
         except Exception as error:
-            self.executor(f"rm -rf {self.target_dir}/{self.artifact_name}.zip")
-            self.executor(f"git -C {self.project_path} reset --hard {local_commit_id}")
+            self._executor(f"rm -rf {self._target_dir}/{self._artifact_name}.zip")
+            self._executor(f"git -C {self._project_path} reset --hard {local_commit_id}")
             raise type(error)(str(error)) from None
 
-    def update_document(self):
-        self.notice("############### UPDATE DOCUMENT ###############")
+    def _update_document(self):
+        self._notice("############### UPDATE DOCUMENT ###############")
         command_list = [
-            f"rm -rf {self.target_dir}/doxygen {self.target_dir}/browser",
-            f"unzip {self.target_dir}/{self.artifact_name}.zip -d {self.target_dir}",
-            f"tar -jxvf {self.target_dir}/foo_doxygen_*.tar.bz2 -C {self.target_dir} >/dev/null",
-            f"tar -jxvf {self.target_dir}/foo_browser_*.tar.bz2 -C {self.target_dir} >/dev/null",
-            f"rm -rf {self.target_dir}/*.zip {self.target_dir}/*.tar.bz2",
+            f"rm -rf {self._target_dir}/doxygen {self._target_dir}/browser",
+            f"unzip {self._target_dir}/{self._artifact_name}.zip -d {self._target_dir}",
+            f"tar -jxvf {self._target_dir}/foo_doxygen_*.tar.bz2 -C {self._target_dir} >/dev/null",
+            f"tar -jxvf {self._target_dir}/foo_browser_*.tar.bz2 -C {self._target_dir} >/dev/null",
+            f"rm -rf {self._target_dir}/*.zip {self._target_dir}/*.tar.bz2",
         ]
         for entry in command_list:
-            _, stderr, return_code = self.executor(entry)
+            _, stderr, return_code = self._executor(entry)
             if stderr or return_code:
-                self.executor(f"rm -rf {self.target_dir}/doxygen {self.target_dir}/browser")
-                self.abort(f"Interrupted due to a failure of the \"{entry}\" command.")
+                self._executor(f"rm -rf {self._target_dir}/doxygen {self._target_dir}/browser")
+                self._abort(f"Interrupted due to a failure of the \"{entry}\" command.")
 
-    def get_redirect_location(self, url, headers):
+    def _get_redirect_location(self, url: str, headers: dict[str, str]) -> str:
         class NoRedirectHandler(urllib.request.HTTPRedirectHandler):
-            def redirect_request(self, *args, **kwargs):  # pylint: disable=unused-argument
+            def redirect_request(self, *args, **kwargs) -> None:  # pylint: disable=unused-argument
                 return None
 
         location = ""
@@ -174,8 +174,8 @@ class Schedule:
 
         return location
 
-    def executor(self, command):
-        self.logger.debug(command)
+    def _executor(self, command: str) -> tuple[str, str, int]:
+        self._logger.debug(command)
         try:
             process = subprocess.run(
                 command,
@@ -193,15 +193,15 @@ class Schedule:
         except subprocess.TimeoutExpired as error:
             return "", str(error), 124
 
-    def notice(self, message):
-        self.logger.info(message)
+    def _notice(self, message: str):
+        self._logger.info(message)
 
-    def abort(self, message):
-        self.logger.warning(message)
+    def _abort(self, message: str):
+        self._logger.warning(message)
         sys.exit(1)
 
 
-def setup_logger(log_level, log_file):
+def create_logger(log_level: int, log_file: str) -> logging.Logger:
     logger = logging.getLogger(__name__)
     logger.setLevel(log_level)
 
@@ -214,7 +214,7 @@ def setup_logger(log_level, log_file):
 
 
 if __name__ == "__main__":
-    LOGGER = setup_logger(logging.INFO, f"/tmp/foo_{os.path.splitext(os.path.basename(__file__))[0]}.log")
+    LOGGER = create_logger(logging.INFO, f"/tmp/foo_{os.path.splitext(os.path.basename(__file__))[0]}.log")
     try:
         Schedule(LOGGER).pull_artifact()
     except Exception:  # pylint: disable=broad-except
