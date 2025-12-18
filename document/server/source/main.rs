f68f78b7b728d0efdb6f0994bc3ef9661fd15ec1
ryftchen <39207065+ryftchen@users.noreply.github.com>
2025-12-19 01:15:23 +0800

    chore: add supervisor support for the archive server

--- document/server/source/main.rs
+++ document/server/source/main.rs
@@ -3,8 +3,10 @@ mod srv;
 mod util;
 
 use std::net::SocketAddr;
+use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::broadcast;
 
-async fn run(args: arg::Args) {
+async fn run(args: arg::Args, mut sig_rx: broadcast::Receiver<()>) {
     let root_dirs = &args.root_dirs;
     let mut srv_group = vec![];
     let mut hint = String::new();
@@ -19,13 +21,26 @@ async fn run(args: arg::Args) {
     }
 
     print!("The archive server starts listening ...\n{}", hint);
+    tokio::spawn(async move {
+        while sig_rx.recv().await.is_ok() {
+            print!("The archive server has started listening ...\n{}", hint);
+        }
+    });
     let _ret = futures_util::future::join_all(srv_group).await;
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let args = arg::parse_args();
+    let (sig_tx, sig_rx) = broadcast::channel(16);
+    let sig_tx = sig_tx.clone();
 
-    run(args).await;
+    tokio::spawn(async move {
+        let mut stream = signal(SignalKind::user_defined1()).unwrap();
+        while stream.recv().await.is_some() {
+            let _ret = sig_tx.send(());
+        }
+    });
+    run(args, sig_rx).await;
     Ok(())
 }
